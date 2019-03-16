dff76718dad62457a3bbf2109e84247b21c2088c
Henrique Ramos <rickeight@hotmail.com>
2019-03-16 20:40:45 -0300

    Updates

---
+++ precache-manifest.154d52f523ce17e76afb85a51b8531a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88714a27c1a6c90148b5793a4b8ad8cb",
+    "url": "/TEMPO1984/static/media/loading.88714a27.gif"
+  },
+  {
+    "revision": "5aa2bf91db82bef9b17f60477181b830",
+    "url": "/TEMPO1984/static/media/FindersKeepers.5aa2bf91.ttf"
+  },
+  {
+    "revision": "e6bedc717c42d8066a53aecf3e1d0a7c",
+    "url": "/TEMPO1984/static/media/ChiKareGo.e6bedc71.ttf"
+  },
+  {
+    "revision": "7e6ddf2420b3898625f1",
+    "url": "/TEMPO1984/static/js/runtime~main.608ce1c8.js"
+  },
+  {
+    "revision": "e11fd4e63b24ad3b1c85",
+    "url": "/TEMPO1984/static/js/main.4d92eaaf.chunk.js"
+  },
+  {
+    "revision": "a5d4db39aa75964ac974",
+    "url": "/TEMPO1984/static/js/2.488050d5.chunk.js"
+  },
+  {
+    "revision": "e11fd4e63b24ad3b1c85",
+    "url": "/TEMPO1984/static/css/main.df9f0db9.chunk.css"
+  },
+  {
+    "revision": "91e03392685558a9f4608a21cb031879",
+    "url": "/TEMPO1984/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4ea22999440ff69eb955be13f30616b.js"
+  "/TEMPO1984/precache-manifest.154d52f523ce17e76afb85a51b8531a0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TEMPO1984/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
