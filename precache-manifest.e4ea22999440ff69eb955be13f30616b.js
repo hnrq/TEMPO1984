64dc6514dba9d36a08bf403cc6f919b4459a1fef
Henrique Ramos <rickeight@hotmail.com>
2019-03-16 20:38:32 -0300

    Updates

---
+++ precache-manifest.e4ea22999440ff69eb955be13f30616b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88714a27c1a6c90148b5793a4b8ad8cb",
+    "url": "/static/media/loading.88714a27.gif"
+  },
+  {
+    "revision": "5aa2bf91db82bef9b17f60477181b830",
+    "url": "/static/media/FindersKeepers.5aa2bf91.ttf"
+  },
+  {
+    "revision": "e6bedc717c42d8066a53aecf3e1d0a7c",
+    "url": "/static/media/ChiKareGo.e6bedc71.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3f3ebc49cb308e913f67",
+    "url": "/static/js/main.65e326a9.chunk.js"
+  },
+  {
+    "revision": "a5d4db39aa75964ac974",
+    "url": "/static/js/2.488050d5.chunk.js"
+  },
+  {
+    "revision": "3f3ebc49cb308e913f67",
+    "url": "/static/css/main.a736d92d.chunk.css"
+  },
+  {
+    "revision": "687ba8dad33a4f2ceec410139a889d56",
+    "url": "/index.html"
+  }
+];
