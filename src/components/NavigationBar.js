65164ef6af09aec78d0e4fe282fb88c681418389
Henrique Ramos <rickeight@hotmail.com>
2019-04-07 18:23:21 -0300

    Commit final

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -32,7 +32,7 @@ export default class Navigation extends Component{
                     <LinkContainer to="/" style={{marginLeft:'-2px'}}><Nav.Link>¤</Nav.Link></LinkContainer>
                     <LinkContainer to="time-left"><Nav.Link className={'text-link'}>Calculadora de vida</Nav.Link></LinkContainer>
                     <LinkContainer to="what-to-do"><Nav.Link className={'text-link'}>Aproveitar mais</Nav.Link></LinkContainer>
-                    <LinkContainer to=""><Nav.Link className={'text-link'}>Viver mais</Nav.Link></LinkContainer>
+                    <LinkContainer to="what-to-do"><Nav.Link className={'text-link'}>Viver mais</Nav.Link></LinkContainer>
                </Nav>
                < Nav pullright="true" className="nav-clock">
                     <span>{this.state.time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</span>
