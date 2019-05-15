bd6c50200ca5daeea8b8e2e5b5ee0158f7ef738e
Henrique Ramos <rickeight@hotmail.com>
2019-05-15 04:35:33 -0300

    Final countdown (Commit alterações Mateus Santos)

---
+++ src/utils/HealthTips.js
@@ -0,0 +1,30 @@
+export const EXERCISE = 1;
+export const FOOD = 2;
+
+export const tips = [
+    {
+        title: 'Faça Musculação',
+        desc: 'Alem de melhorar sua aparencia fisica, "puxar ferro" pode <a href="https://www.healthline.com/health-news/weight-training-can-help-people-with-type-2-diabetes">reduzir o risco de diabetes</a>, <a href="https://www.health.harvard.edu/mind-and-mood/strengthen-your-mood-with-weight-training">melhorar seu humor</a> e <a href="https://www.medicalnewstoday.com/articles/323732.php">melhorar sua saude cardiaca</a>. Ta esperando o que? Vem malhar! VEM MONSTRO!',
+        category: EXERCISE
+    },
+    {
+        title: 'Comece a correr',
+        desc: 'Esta atividade ajuda a perder peso, <a href="https://www.ncbi.nlm.nih.gov/pubmed/7848025">pode ajudar a aumentar os níveis do colesterol bom</a> e é uma atividade que ajuda a aliviar o stress. O que ta esperando? Levanta da cadeira e vai correr um pouco!',
+        category: EXERCISE
+    },
+    {
+        title: 'Faça natação',
+        desc: 'Nadar ajuda a tonificar os musculos, queimar calorias e <a href="https://www.ncbi.nlm.nih.gov/pubmed/1470792">auxilia pessoas com asma, pois pode ajudar a expandir a capacidade pulmonar</a>. Comece a nadar agora e quem sabe, um dia, vira uma sereia (ou tritao).',
+        category: EXERCISE
+    },
+    {
+        title: 'Come verdura, menino!',
+        desc: 'Voce pode ter ouvido isso na sua infancia e, sim, voce tem que comer verduras. Vegetais podem <a href="https://www.hsph.harvard.edu/nutritionsource/what-should-you-eat/vegetables-and-fruits/">reduzir o risco de doencas cardiacas e derrame, regular a pressao cardiovascular</a> e <a href="https://www.google.com/search?ei=W6TbXJflOKHA5OUP4PiEoAM&q=cancer+vegetables+harvard&oq=cancer+vegetables+harvard&gs_l=psy-ab.3..0i22i30.8086.10154..10227...0.0..0.115.768.0j7......0....1..gws-wiz.......0i203.pnO_kf20qDQ">o risco de cancer de mama</a>.',
+        category: FOOD
+    },
+    {
+        title: 'I have a pen, I have a Pineapple',
+        desc: 'Abacaxis, alem de serem ricos em nutrientes como as vitaminas C e B6, <a href="https://www.ncbi.nlm.nih.gov/pubmed/24959679">possuem antioxidantes</a> que ajudam a combater doenças.',
+        category: FOOD
+    },
+]
