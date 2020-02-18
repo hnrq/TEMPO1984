732f47887bda13bcb86ca11f03b168c4d9754112
Henrique Ramos <henriquealberone@outlook.com>
2020-02-18 18:07:40 -0300

    Updates

---
+++ precache-manifest.28524d8d376c935aa8bcf4f8f2a3d56f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5bb58f723cc365818d567a8932024",
+    "url": "/TEMPO1984/index.html"
+  },
+  {
+    "revision": "1647c04fc90d0b98d019",
+    "url": "/TEMPO1984/static/css/main.375b9ae2.chunk.css"
+  },
+  {
+    "revision": "af60ea466dafb7eea8a0",
+    "url": "/TEMPO1984/static/js/2.c097ea6a.chunk.js"
+  },
+  {
+    "revision": "ca5b2c05011b8c026390333604cd2198",
+    "url": "/TEMPO1984/static/js/2.c097ea6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1647c04fc90d0b98d019",
+    "url": "/TEMPO1984/static/js/main.055aeed2.chunk.js"
+  },
+  {
+    "revision": "c1206886f713adc85409",
+    "url": "/TEMPO1984/static/js/runtime-main.edd4cbe4.js"
+  },
+  {
+    "revision": "e6bedc717c42d8066a53aecf3e1d0a7c",
+    "url": "/TEMPO1984/static/media/ChiKareGo.e6bedc71.ttf"
+  },
+  {
+    "revision": "5aa2bf91db82bef9b17f60477181b830",
+    "url": "/TEMPO1984/static/media/FindersKeepers.5aa2bf91.ttf"
+  },
+  {
+    "revision": "88714a27c1a6c90148b5793a4b8ad8cb",
+    "url": "/TEMPO1984/static/media/loading.88714a27.gif"
+  }
+]);
