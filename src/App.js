45bc42e81ec965dd9bcfd9711c4a428c2d2e43c0
Henrique Ramos <henriquealberone@outlook.com>
2020-02-18 18:06:08 -0300

    Add background and change router to Hash

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
 import NavBar from './components/NavigationBar';
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Container from './components/Container';
 
 class App extends Component {
-  render() {
-    return (
-      < BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div className="App">
-          <NavBar/>
-          <Container/>
-        </div>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<HashRouter basename="/">
+				<div className="App">
+					<NavBar />
+					<Container />
+				</div>
+			</HashRouter>
+		);
+	}
 }
 
 export default App;
